fcb505d91bf0f3f262c4a1e5a25bc4dedfc0fffb
Dorien <dorien.drees@gmail.com>
2025-12-14 19:43:10 +0100

    Remove debug statements

--- src/support/queercal_ticket_warning.erl
+++ src/support/queercal_ticket_warning.erl
@@ -7,16 +7,10 @@
   
 event(#submit{message={set_skip_ticket_warning, TicketUrl}}, Context) ->
     Hide = z_context:get_q(<<"is_hide_disclaimer">>, Context),
-
-    ?DEBUG(m_server_storage:lookup(Context)),
     
     case Hide of
         <<"on">> ->
-            z_notifier:first({server_storage, store, <<"skip_ticket_warning">>, true}, Context),
-            % z_context:set_q(<<"skip_ticket_warning">>, true, Context),
-            ?DEBUG(z_context:get_q(<<"z_language">>, Context));
-
-            % z_context:set_session(<<"skip_ticket_warning">>, true, Context);
+            z_notifier:first({server_storage, store, <<"skip_ticket_warning">>, true}, Context);
         _ ->
             ok
     end,
